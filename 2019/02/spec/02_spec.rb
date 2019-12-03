c9053e5d77fa5a5c029f9389e3f0ca5df0a83766
Helen Griffiths <threepistons@users.noreply.github.com>
2019-12-03 01:52:01 +0000

    Day two complete.

--- 2019/02/spec/02_spec.rb
+++ 2019/02/spec/02_spec.rb
@@ -1,17 +1,19 @@
 require '02'
 
 RSpec.describe Intcode, "#1202" do
-  context 'with supplied test values' do
+  context 'with supplied values' do
     it 'tests my Intcode computer' do
       ic = Intcode.new
-      ic.calculate('1,0,0,0,99')
+      ic.loopcalculate('1,0,0,0,99')
       expect(ic.result).to eq '2,0,0,0,99'
-      ic.calculate('2,3,0,3,99')
+      ic.loopcalculate('2,3,0,3,99')
       expect(ic.result).to eq '2,3,0,6,99'
-      ic.calculate('2,4,4,5,99,0')
+      ic.loopcalculate('2,4,4,5,99,0')
       expect(ic.result).to eq '2,4,4,5,99,9801'
-      ic.calculate('1,1,1,4,99,5,6,0,99')
+      ic.loopcalculate('1,1,1,4,99,5,6,0,99')
       expect(ic.result).to eq '30,1,1,4,2,5,6,0,99'
+      ic.loopcalculate('1,1,1,4,99,5,6,0,99,7,3,0')
+      expect(ic.result).to eq '30,1,1,4,2,5,6,0,99,7,3,0'
     end
   end
 end
