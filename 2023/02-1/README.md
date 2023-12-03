9a0cfaa01bbc6849734b4f66652d0827e9756992
Helen Griffiths <16269977+threepistons@users.noreply.github.com>
2023-12-03 00:42:31 +0000

    02-1

---
+++ 2023/02-1/README.md
@@ -0,0 +1,5 @@
+# Method
+
+For each game:
+10. For each colour, check that the number of cubes drawn never exceeds the ceiling.
+20. If any colour exceeds the ceiling, discard this game id, else add the game id to the game id total.
---
+++ 2023/02-1/advent_test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import re
+import string
+
+def test_answer():
+    assert cubecounter('test.txt') == 8
+
+def cubecounter(document):
+    colours = {'red': 12, 'green': 13, 'blue': 14}
+    sum = 0
+    with open(document) as d:
+        # make a list of all the lines
+        text = d.readlines()
+    for line in text:
+        a = re.compile(r"\d+")
+        # the first integers in each line are the game id
+        b = a.search(line)
+        gameid = int(b.group(0))
+        # print(gameid)
+        # Now the harder bit
+        # get the line after the colon
+        game = line.partition(':')[2]
+        # print(game)
+        # and make another dict
+        c = re.compile(r"[;,]")
+        e = re.split(c, game)
+        # print(e)
+        # e is a list of strings like " 23 red"
+        greenflag = True
+        for f in e:
+            draw = f.strip().partition(" ")
+            # draw is a tuple like ('23', ' ', 'red')
+            for colour, ceiling in colours.items():
+                if colour == draw[2] and int(ceiling) < int(draw[0]):
+                    greenflag = False
+        if greenflag == True:
+            sum += gameid
+    return(sum)
+
+result = cubecounter('input.txt')
+
+print(result)
